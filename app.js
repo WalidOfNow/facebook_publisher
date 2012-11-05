c46882dd8a539d21bbcaf94efb172856c3a1b418
walid-wah <walid.wah@gmail.com>
2012-11-05 21:08:47 +0200

    add facebook_auth

--- app.js
+++ app.js
@@ -5,10 +5,13 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , auth = require('./routes/auth')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
+ 
 
+ 
 var app = express();
 
 app.configure(function(){
@@ -21,14 +24,23 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(express.session());
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
+/*var sess = function(req,res){
+req.session = {id : ''};
+}
+app.use(sess);*/
+//app.get('/',  function(req,res,next){
+//if(req.id){next()}else{res.redirect('/auth/facebook')}},routes.index); 
 
-app.get('/', routes.index);
-app.get('/users', user.list); 
+app.get('/auth/facebook', auth.facebook);
+
+ 
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
---
+++ routes/auth.js
@@ -0,0 +1,47 @@
+var graph = require('fbgraph');
+  
+var fb_conf = {
+    client_id:      '429251933799377'
+  , client_secret:  'f3c648e68556196a2e99282cd3dd6c53'
+  , scope:          'email, user_about_me, user_birthday, user_location, publish_stream'
+  , redirect_uri:   'http://localhost:3000/auth/facebook'
+};
+
+
+
+exports.facebook = function(req, res) {
+
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) { 
+    var authUrl = graph.getOauthUrl({
+        "client_id":     fb_conf.client_id
+      , "redirect_uri":  fb_conf.redirect_uri
+      , "scope" :         fb_conf.scope
+    });
+
+    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {  //req.query.error == 'access_denied'
+      res.send('access denied');
+    }
+    return;
+  }
+  // code is set
+  // we'll send that and get the access token
+   graph.authorize({
+      "client_id":      fb_conf.client_id
+    , "redirect_uri":   fb_conf.redirect_uri
+    , "client_secret":  fb_conf.client_secret
+    , "code":           req.query.code
+  }, function (err, facebookRes) {
+     
+      //var accessToken = facebookRes.access_token;
+     // req.session = {access_token : accessToken , auth : 'true'};
+ // graph.get("me", function(err, res) {
+  //res.redirect('/');
+//});
+  });
+
+  };
+
