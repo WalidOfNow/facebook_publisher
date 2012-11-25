3f361cf3c0319f40293b6c0b0e8040bbfade2425
walid-wah <walid.wah@gmail.com>
2012-11-25 08:13:23 +0200

    add groups sellector function

--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,18 @@ exports.post= function(req,res){
     var wallPost = {
         message: req.body.text
     };
+ //  var  i=0;
+   // var groups= {};
+    /*req.body.checkbox.forEach(function(item){
+      //  if (item.checked = true) {
+            console.log(item[i]);
+      //  }
+    })*/
+    for (var i in req.body.checkbox) {
+        console.log(i);
+    }
+
+
     if (wallPost.message) {
         graph.post("me/feed", wallPost, function (err, res) {
             console.log(res);
